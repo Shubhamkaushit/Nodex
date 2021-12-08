09f585689a23f2acce21bcea3815542062130422
Shubhamkaushit <shubham.kaushit@gmail.com>
2021-12-09 00:42:27 +0530

    Modified file.js

--- file.js
+++ file.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', function(req, res){
+
+    res.send("Hello Bro!!!");
+});
+
+app.get('/alien', function(req, res){
+
+    const id = req.query.id;
+    res.send("Hello alien "+id);
+});
+
+app.get('/alien/:id', function(req, res){
+
+    const id = req.params.id;
+    res.send("Hello Alien: "+id);
+});
+
 app.listen(9000, function(req, res){
 
     console.log("Hello World!!");
